5825c7583314f59ee13f13066480be4190128b31
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-07-16 18:11:18 +0100

    Initial project added. Closes #1

---
+++ src/main/java/haydende/springrecipeapp/SpringrecipeappApplication.java
@@ -0,0 +1,13 @@
+package haydende.springrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrecipeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringrecipeappApplication.class, args);
+    }
+
+}
